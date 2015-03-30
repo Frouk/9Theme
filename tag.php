f49a7188511e2bd9b2f69f1bbb8d641375e64e8c
Aris <arisfkiaras@gmail.com>
2015-03-30 21:36:43 +0300

    Single tag estw done

---
+++ tag.php
@@ -0,0 +1,30 @@
+<?php  get_header(); ?>
+
+<div id="container">
+	<div id="main">
+		<div id="posts">
+			<?php $yo=single_tag_title("", false); query_posts('tag='.$yo.'&paged=' . get_query_var('paged'));?>
+       			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+				<div id="singlepost">
+        				<div id="posttitle">
+						<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+					</div>
+					<div id="postcontent">
+						<a href="<?php the_permalink(); ?>"><?php the_content(); ?></a>
+					</div>
+					<div id="postdata">
+						<div id="stats">900 points - <?php comments_number( 'No comments', 'One comment', '% comments' ); ?></div>
+						<div class="vote">
+							<li><a class="voteup" href="http://google.com"></a></li>
+							<li><a class="votedown" href="http://google.com"></a></li>
+						</div>
+					</div>
+				</div>
+
+
+			<?php endwhile; else: ?>
+				<?php _e('It seems there are no posts here.'); ?></p>
+			<?php endif; ?>
+		</div>
+	</div>
+<?php get_footer(); ?>

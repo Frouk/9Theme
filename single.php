9fec7e1e249378593074c7c3971028d450d301f2
Aris <arisfkiaras@gmail.com>
2015-06-24 18:51:05 +0300

    Working on theme style,will make all php files now and design many styles.css later

--- single.php
+++ single.php
@@ -6,16 +6,16 @@
 			<div id="pp_post">
 				<div id="pp_title"><?php the_title(); ?></div>
 				<div id="pp_content"><?php the_content(); ?></div>
-				<div id="pp_data"></div>
-				<div id="pp_comments"></div>
-				<div id="postdata">
-					<div id="stats">900 points - <?php comments_number( 'No comments', 'One comment', '% comments' ); ?> By: <?php echo the_author_posts_link(); ?></div>
-				
-					<div class="vote">
-						<li><a class="voteup" href="javascript:void(0);" onclick="vote(<?php echo get_the_ID(); ?>,1);"></a></li>
-						<li><a class="votedown" href="javascript:void(0);" onclick="vote(<?php echo get_the_ID(); ?>,2);"></a></li>
+				<div id="pp_data">
+						<div id="stats">900 points - <?php comments_number( 'No comments', 'One comment', '% comments' ); ?>
+							<div id="tags"> <?php the_tags( 'Tags:', ',', '.' ); ?> </div>
+						</div>
+						<div class="vote">
+							<li><a class="voteup" href="javascript:void(0);" onclick="vote(<?php echo get_the_ID(); ?>,1);"></a></li>
+							<li><a class="votedown" href="javascript:void(0);" onclick="vote(<?php echo get_the_ID(); ?>,2);"></a></li>
+						</div>
 					</div>
-				</div>
+				<div id="pp_comments"></div>	
 			</div>
 		<?php endwhile; /* end loop */ ?>
 		<?php comments_template( '', true ); ?>
--- tag.php
+++ tag.php
@@ -3,8 +3,10 @@
 <div id="container">
 	<div id="main">
 		<div id="posts">
+			<div id="TagTitle">Tag: <?php single_tag_title(); ?></div>
 			<?php $yo=single_tag_title("", false); query_posts('tag='.$yo.'&paged=' . get_query_var('paged'));?>
-       			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+       		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+				
 				<div id="singlepost">
         				<div id="posttitle">
 						<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
@@ -13,18 +15,26 @@
 						<a href="<?php the_permalink(); ?>"><?php the_content(); ?></a>
 					</div>
 					<div id="postdata">
-						<div id="stats">900 points - <?php comments_number( 'No comments', 'One comment', '% comments' ); ?></div>
+						<div id="stats">900 points - <?php comments_number( 'No comments', 'One comment', '% comments' ); ?>
+							<div id="tags"> <?php the_tags( 'Tags:', ',', '.' ); ?> </div>
+						</div>
 						<div class="vote">
-							<li><a class="voteup" href="http://google.com"></a></li>
-							<li><a class="votedown" href="http://google.com"></a></li>
+							<li><a class="voteup" href="javascript:void(0);" onclick="vote(<?php echo get_the_ID(); ?>,1);"></a></li>
+							<li><a class="votedown" href="javascript:void(0);" onclick="vote(<?php echo get_the_ID(); ?>,2);"></a></li>
 						</div>
 					</div>
 				</div>
 
-
 			<?php endwhile; else: ?>
 				<?php _e('It seems there are no posts here.'); ?></p>
 			<?php endif; ?>
+
+			<div id="Paging">
+				<div id="NewerPages"><?php previous_posts_link('&laquo; Newer Entries') ?></div>
+				<div id="OlderPages"><?php next_posts_link('Older Entries &raquo;') ?></div>
+			</div>
 		</div>
+		<?php get_sidebar(); ?>
 	</div>
+
 <?php get_footer(); ?>

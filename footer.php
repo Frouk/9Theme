21b34b8620b01df17b7b350b7b1712967c5dce93
Your Name <arisfkiaras@gmail.com>
2015-07-06 16:36:45 +0300

    testing images folder

--- footer.php
+++ footer.php
@@ -2,6 +2,9 @@
 <footer id="bot-nav">
 	<div class="nav-wrap"><nav>
 		<div id="footermenu"><?php wp_nav_menu( array( 'theme_location' => 'footer-menu' ) ); ?></div>
+		<div id="frouk">
+			<a href="https://github.com/Frouk/9Theme">9Theme by Frouk</a>
+		</div>
 	</nav></div>
 </footer>
 

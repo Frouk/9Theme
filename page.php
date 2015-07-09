ca1e19f5c6cf27c56360263b0218a92a5f392209
Your Name <arisfkiaras@gmail.com>
2015-07-09 12:06:13 +0300

    adding page.php

---
+++ page.php
@@ -0,0 +1,3 @@
+<?php  get_header(); ?><div id="container">	<div id="main">		<div id="pagecontent">			<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>							<?php the_content(); ?>			<?php endwhile; ?>		</div>
+		<?php get_sidebar(); ?>	</div>
+<?php get_footer(); ?>
